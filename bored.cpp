72e8b878a7c30bde7bbbd551a3aca25309547a8c
lethe <lethe@Dreadnaught.austin.rr.com>
2019-01-30 23:48:22 -0600

    comment

---
+++ bored.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int x = 1;
+
+	while ( x != 0)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			if (i == 0)
+			{
+				cout << "B" << "ored ";
+			}
+			else if (i == 1)
+			{
+				cout << "b" << "O" << "red ";
+			}
+			else if (i == 2)
+			{
+				cout << "bo" << "R" << "ed ";
+			}
+			else if (i == 3)
+			{
+				cout << "bor" << "E" << "d ";
+			}
+			else if (i == 4)
+			{
+				cout << "bore" << "D ";
+			}
+		}
+	}
+
+}
